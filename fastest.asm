3e6a9f341b6e84a8e6b144d85509a07466d0420a
Denys Kozyr <dkozyr@gmail.com>
2021-09-28 21:39:25 +0300

    brackets optimization

--- fastest.asm
+++ fastest.asm
@@ -42,8 +42,8 @@ _nothing_to_print:  xor     RAX, RAX                    ; 0 - sys_read
 op_open:            cmp     byte [RBX], 0
                     jz      $+1000000
 op_close:           cmp     byte [RBX], 0
-                    jnz     $-1000000
-op_set_zero:        mov     byte [RBX], 0
+op_close_short:     jnz     $-1000000
+op_set_value:       mov     byte [RBX], 0
 op_eos:             mov     RDX, RDI
                     sub     RDX, output                 ; length
                     jz      _ret_without_print
@@ -62,8 +62,9 @@ OP_PTR_SUB_SIZE     equ     (op_dot - op_ptr_sub)
 OP_OUTPUT_SIZE      equ     (op_comma - op_dot)
 OP_INPUT_SIZE       equ     (op_open - op_comma)
 OP_OPEN_SIZE        equ     (op_close - op_open)
-OP_CLOSE_SIZE       equ     (op_set_zero - op_close)
-OP_SET_ZERO_SIZE    equ     (op_eos - op_set_zero)
+OP_CLOSE_SIZE       equ     (op_set_value - op_close)
+OP_CLOSE_SHORT_SIZE equ     (op_set_value - op_close_short)
+OP_SET_VALUE_SIZE   equ     (op_eos - op_set_value)
 OP_EOS_SIZE         equ     (fd - op_eos)
 
 ID_EOS              equ     0
@@ -75,7 +76,11 @@ ID_INPUT            equ     5
 ID_OUTPUT           equ     6
 ID_OPEN             equ     7
 ID_CLOSE            equ     8
-ID_SET_ZERO         equ     9
+ID_CLOSE_SHORT      equ     ID_CLOSE + 128
+ID_SET_VALUE        equ     10
+
+QWORD_SIZE          equ     8
+OPERAND_INFO_SIZE   equ     2 * QWORD_SIZE
 
 ; ------------------------------------------------------------------------------
 _start:             xor     RAX, RAX
@@ -126,7 +131,6 @@ _exit:              mov     RAX, 60                     ; exit
 %macro process_script_symbol 3
                     cmp     AL, %1
                     jnz     .test_next_%2
-
                     add     RDX, %3
                     mov     AL, %2
                     jmp     _good_symbol
@@ -146,7 +150,7 @@ _exit:              mov     RAX, 60                     ; exit
                     jz      .symbol_counter_%2
 
                     and     RAX, 0xFF
-                    mov     [RBX + 8], RAX
+                    mov     [RBX + QWORD_SIZE], RAX
 
                     add     RDX, %3
                     mov     AL, %2
@@ -166,8 +170,21 @@ _exit:              mov     RAX, 60                     ; exit
                     cmp     AH, ']'
                     jnz     .test_next_%1
 
-                    add     RSI, 2
-                    sub     RCX, 2
+                    inc     RSI
+
+                    xor     AL, AL
+.counter_next_%1:   inc     RSI
+                    mov     AH, [RSI + 1]
+                    inc     AL
+                    cmp     AH, '+'
+                    jz      .counter_next_%1
+                    sub     AL, 2
+                    cmp     AH, '-'
+                    jz      .counter_next_%1
+                    inc     AL
+
+                    and     RAX, 0xFF
+                    mov     [RBX + QWORD_SIZE], RAX
 
                     add     RDX, %2
                     mov     AL, %1
@@ -187,7 +204,7 @@ _process_symbol:    mov     AL, [RSI]
                     or      AL, AL
                     jz      _end_of_stream
 
-                    process_script_open_minus_close ID_SET_ZERO, OP_SET_ZERO_SIZE
+                    process_script_open_minus_close ID_SET_VALUE, OP_SET_VALUE_SIZE
 
                     process_script_symbol_with_counter '+', ID_ADD,     OP_ADD_SIZE
                     process_script_symbol_with_counter '-', ID_SUB,     OP_SUB_SIZE
@@ -197,7 +214,21 @@ _process_symbol:    mov     AL, [RSI]
                     process_script_symbol '.', ID_OUTPUT,  OP_OUTPUT_SIZE
                     process_script_symbol ',', ID_INPUT,   OP_INPUT_SIZE
                     process_script_symbol '[', ID_OPEN,    OP_OPEN_SIZE
-                    process_script_symbol ']', ID_CLOSE,   OP_CLOSE_SIZE
+
+                    cmp     AL, ']'
+                    jnz     _bad_symbol
+
+                    cmp     byte [RDI - 1], ID_ADD
+                    jz      _good_close_short
+                    cmp     byte [RDI - 1], ID_SUB
+                    jz      _good_close_short
+                    jmp     _test_close
+
+_good_close_short:  mov     AL, ID_CLOSE_SHORT
+                    add     RDX, OP_CLOSE_SHORT_SIZE
+                    jmp     _good_symbol
+
+_test_close:        process_script_symbol ']', ID_CLOSE,   OP_CLOSE_SIZE
 
                     jmp     _bad_symbol
 
@@ -205,7 +236,7 @@ _good_symbol:       mov     [RDI], AL
                     inc     RDI
 
                     mov     [RBX], RDX
-                    add     RBX, 2 * 8
+                    add     RBX, OPERAND_INFO_SIZE
 
 _bad_symbol:        inc     RSI
                     jnz     _process_symbol
@@ -220,9 +251,10 @@ _end_of_stream:     mov     byte [RDI], ID_EOS
                     push    RDX
 
                     mov     RDX, 1                      ; find corresponding bracket on RDX = 0
-    %1_next:        %1      RBX, 2 * 8
+    .next_%1:       %1      RBX, OPERAND_INFO_SIZE
                     %2      RSI
                     mov     AH, [RSI]
+                    and     AH, 0x3F
                     cmp     AH, %3
                     jnz     .skip1
                     inc     RDX
@@ -230,7 +262,7 @@ _end_of_stream:     mov     byte [RDI], ID_EOS
                     jnz     .skip2
                     dec     RDX
     .skip2:         or      RDX, RDX
-                    jnz     %1_next
+                    jnz     .next_%1
 
                     mov     RCX, [RBX]
                     pop     RDX
@@ -252,7 +284,7 @@ process_next_%2:
                     cmp     AL, %2
                     jnz     process_next_%2
 
-                    mov     RCX, [RBX + 8]
+                    mov     RCX, [RBX + QWORD_SIZE]
                     mov     RSI, %3 + %4 - %1
 %if %1 == 1
                     mov     byte [RSI], CL
@@ -265,12 +297,12 @@ process_next_%2:
 process_next_%2:
 %endmacro
 
-prepare_the_code:   mov     RBX, operand_size - 2 * 8
+prepare_the_code:   mov     RBX, operand_size - OPERAND_INFO_SIZE
                     mov     RDI, the_code
                     mov     RDX, script - 1
 
 _next_symbol:       inc     RDX
-                    add     RBX, 2 * 8
+                    add     RBX, OPERAND_INFO_SIZE
                     mov     AL, [RDX]
 
                     process_script_symbol_to_code_with_variable 1, ID_ADD, op_add, OP_ADD_SIZE
@@ -282,7 +314,7 @@ _next_symbol:       inc     RDX
                     process_script_symbol_to_code ID_OUTPUT, op_dot,   OP_OUTPUT_SIZE
                     process_script_symbol_to_code ID_INPUT,  op_comma, OP_INPUT_SIZE
 
-                    process_script_symbol_to_code ID_SET_ZERO, op_set_zero, OP_SET_ZERO_SIZE
+                    process_script_symbol_to_code_with_variable 1, ID_SET_VALUE, op_set_value, OP_SET_VALUE_SIZE
 
                     cmp     AL, ID_OPEN
                     jnz     _check_close
@@ -295,13 +327,23 @@ _next_symbol:       inc     RDX
                     jmp     _copy_and_process_next_symbol
 
 _check_close:       cmp     AL, ID_CLOSE
-                    jnz     _check_eos
+                    jnz     _check_close_short
 
                     find_offset sub, dec, ID_CLOSE, ID_OPEN
 
                     mov     dword [op_close + OP_CLOSE_SIZE - 4], ECX
                     mov     RSI, op_close
                     mov     RCX, OP_CLOSE_SIZE
+                    jmp     _copy_and_process_next_symbol
+
+_check_close_short: cmp     AL, ID_CLOSE_SHORT
+                    jnz     _check_eos
+
+                    find_offset sub, dec, ID_CLOSE, ID_OPEN
+
+                    mov     dword [op_close_short + OP_CLOSE_SHORT_SIZE - 4], ECX
+                    mov     RSI, op_close_short
+                    mov     RCX, OP_CLOSE_SHORT_SIZE
                     jmp     _copy_and_process_next_symbol
 
 _check_eos:         cmp     AL, ID_EOS
