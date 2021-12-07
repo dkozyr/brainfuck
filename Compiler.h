c08ec06a1ab5be688855f1ed5587d61dea3ee01d
Denys Kozyr <dkozyr@gmail.com>
2021-12-08 00:22:25 +0200

    integration tests

--- Compiler.h
+++ Compiler.h
@@ -1,17 +1,89 @@
 #pragma once
 
-#include "Operand.h"
+#include "Executor.h"
+#include "ExecutorAsm.h"
 
 class Compiler {
 public:
     Compiler() = default;
 
-    void Process(const char* begin, const char* end);
-    void ExecuteOptimized();
-    void Execute();
+    void Process(const char* begin, const char* end) {
+        while(begin != end) {
+            switch(*begin) {
+                case '+': Process(Add{}); break;
+                case '-': Process(Sub{}); break;
+                case '>': Process(PtrAdd{}); break;
+                case '<': Process(PtrSub{}); break;
+                case '.': Process(Output{}); break;
+                case ',': Process(Input{}); break;
+                case '[':
+                    Process(WhileBegin{});
+                    break;
+                case ']':
+                    if(_program.size() >= 2) {
+                        const auto idx = _program.size() - 2;
+                        const auto open = std::get_if<WhileBegin>(&_program[idx]);
+                        if(open) {
+                            const auto sub = std::get_if<Sub>(&_program[idx + 1]);
+                            if(sub && (sub->count == 1)) {
+                                _program.pop_back();
+                                _program.pop_back();
+                                Process(Set{});
+                                break;
+                            }
+
+                            const auto add = std::get_if<Add>(&_program[idx + 1]);
+                            if(add && (add->count == 1)) {
+                                _program.pop_back();
+                                _program.pop_back();
+                                Process(Set{});
+                                break;
+                            }
+
+                        }
+                    }
+                    Process(WhileEnd{});
+                    break;
+            }
+            begin++;
+        }
+    }
+
+    void ExecuteOptimized() { ExecutorAsm::Run(_program); }
+    void Execute() { Executor::Run(_program); }
 
 private:
-    void Process(const Operand& operand);
+    void Process(const Operand& operand) {
+        if(_program.empty()) {
+            _program.push_back(operand);
+            return;
+        }
+
+        auto& prev = _program.back();
+        std::visit(overloaded {
+            [&](const Add& x) {
+                if(auto v = std::get_if<Set>(&prev); v) {
+                    v->value++;
+                } else {
+                    CheckOperandWithPreviousAndInsert<Add, Sub>(x, prev);
+                }
+            },
+            [&](const Sub& x) {
+                if(auto v = std::get_if<Set>(&prev); v) {
+                    v->value--;
+                } else {
+                    CheckOperandWithPreviousAndInsert<Sub, Add>(x, prev);
+                }
+            },
+            [&](const PtrAdd& x) { CheckOperandWithPreviousAndInsert<PtrAdd, PtrSub>(x, prev); },
+            [&](const PtrSub& x) { CheckOperandWithPreviousAndInsert<PtrSub, PtrAdd>(x, prev); },
+            [&](const Set& x) { _program.push_back(x); },
+            [&](const WhileBegin& x) { _program.push_back(x); },
+            [&](const WhileEnd& x) { _program.push_back(x); },
+            [&](const Output& x) { _program.push_back(x); },
+            [&](const Input& x) { _program.push_back(x); },
+        }, operand);
+    }
 
     template<typename TSame, typename TOpposite>
     void CheckOperandWithPreviousAndInsert(const Operand& current, Operand& previous) {
