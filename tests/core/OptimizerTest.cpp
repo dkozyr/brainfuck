cd12703019f5a4862c66ad4d60913e704f93e97b
Denys Kozyr <dkozyr@gmail.com>
2021-12-11 18:06:50 +0200

    tests folder refactoring

--- tests/core/OptimizerTest.cpp
+++ tests/core/OptimizerTest.cpp
@@ -1,4 +1,4 @@
-#include "../Optimizer.h"
+#include "../../Optimizer.h"
 
 #include <gtest/gtest.h>
 
---
+++ tests/operand_writer/OptimizerTest.cpp
@@ -0,0 +1,9 @@
+#include "../../operand_writer/OperandWriter.h"
+
+#include <gtest/gtest.h>
+
+#include <iostream>
+
+TEST(Optimizer, Basic) {
+    std::cout << "hello world" << std::endl;
+}
