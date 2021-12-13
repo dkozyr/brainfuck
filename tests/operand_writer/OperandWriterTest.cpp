cbf908b5a38c3c1c987ebd3a33d3c63bfd94cb81
Denys Kozyr <dkozyr@gmail.com>
2021-12-14 00:35:45 +0200

    code tidy up

--- tests/operand_writer/OperandWriterTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "../../operand_writer/OperandWriter.h"
-
-#include <gtest/gtest.h>
-
-#include <iostream>
-
-TEST(OperandWriterTest, Basic) {
-    std::cout << "hello world" << std::endl;
-}
